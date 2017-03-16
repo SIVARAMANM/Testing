8f4b75cc7ebb4ff9868ad298b077c13fdb57da8c
lnc <lnc>
2017-03-16 13:11:46 +0530

    devops

---
+++ DevOps_20Feb_2017/Fact_TDD/src/Maths.java
@@ -0,0 +1,26 @@
+
+public class Maths {
+	
+	public int getFact(int num)
+	{
+		if(num<0)
+		{
+			throw new IllegalArgumentException();
+		}
+		if(num==0)
+		{
+			return 1;
+		}
+		
+		return 1;
+	}
+	
+	public void timeoutMethod()
+	{
+		for(int i=1;i<=1000;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
